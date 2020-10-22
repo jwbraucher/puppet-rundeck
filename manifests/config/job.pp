8afb62dd9d7bef32b142383849b8cb204fadc4c0
Jeff Braucher <jeffrey.braucher@garmin.com>
2020-10-21 21:29:26 -0600

    Allow job name to be overrideen. Add example.

--- manifests/config/job.pp
+++ manifests/config/job.pp
@@ -21,6 +21,10 @@
 # [*title*]
 #   The name of the job to add or delete
 #
+# [*job_name*]
+#   Use this to customize the job name, or to have two jobs with the same name (e.g. group1/foo and group2/foo)
+#   (defaults to $title)
+#
 # [*group*]
 #   The group of the job to add or delete (defautls to an empty string, which means the root group)
 #
@@ -28,12 +32,21 @@
 #
 # Create a job:
 #
-# rundeck::config::job { 'my-job':
-#  name   => 'my-job',
-#  job_definition => 'my_module/my_job.yaml'
+# rundeck::config::job { 'foo-job':
+#  job_name   => 'Foo',
+#  group => 'Operations',
+#  job_definition => 'my_module/foo-job.yaml'
 # }
 #
+# rundeck::config::job { 'delete-bar-job':
+#  job_name   => 'Bar',
+#  group => 'Operations',
+#  ensure => absent
+# }
+#
+#
 define rundeck::config::job(
+  String $job_name = $title,
   String $project,
   String $job_definition = '',
   String $format = 'yaml',
@@ -49,32 +62,32 @@ define rundeck::config::job(
   if $ensure == 'present' {
 
     # create job
-    file { "$title":
+    file { "$job_name":
       ensure  => file,
       path    => "${jobs_dir}/${job_filename}",
       content => template($job_definition),
     }
-    -> exec { "$title":
+    -> exec { "$job_name":
       path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
       command => "rd jobs load --remove-uuids --duplicate update --format ${format} --project ${project} --file ${jobs_dir}/${job_filename}",
       user => "${rundeck::user}",
       environment => [ "HOME=${rundeck::rdeck_home}" ],
       tries => 300,
       try_sleep => 1,
-      onlyif => "rd jobs list --project ${project} --groupxact ${group} --jobxact ${title} | grep -q '0 Jobs'"
+      onlyif => "rd jobs list --project ${project} --groupxact ${group} --jobxact ${job_name} | grep -q '0 Jobs'"
     }
 
   }
   elsif $ensure == 'absent' {
 
-    exec { "$title":
+    exec { "$job_name":
       path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
-      command => "rd jobs purge --confirm --project ${project} --jobxact ${title} --groupxact ${group}",
+      command => "rd jobs purge --confirm --project ${project} --jobxact ${job_name} --groupxact ${group}",
       user => "${rundeck::user}",
       environment => [ "HOME=${rundeck::rdeck_home}" ],
       tries => 90,
       try_sleep => 1,
-      onlyif => "rd jobs list --project ${project} --groupxact ${group} --jobxact ${title} | grep -q '1 Jobs'"
+      onlyif => "rd jobs list --project ${project} --groupxact ${group} --jobxact ${job_name} | grep -q '1 Jobs'"
     }
 
   }
